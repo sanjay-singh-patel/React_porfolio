2ad63475aa3d0cccb0458973eaef177186df411e
sanjay-singh-patel <sanjupatel11112000@gmail.com>
2022-06-02 06:06:28 +0000

    .

--- src/components/icons/icon.js
+++ src/components/icons/icon.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {
   IconAppStore,
   IconBookmark,
-  IconCodepen,
+  IconLeetcode,
   IconExternal,
   IconFolder,
   IconFork,
@@ -23,8 +23,8 @@ const Icon = ({ name }) => {
       return <IconAppStore />;
     case 'Bookmark':
       return <IconBookmark />;
-    case 'Codepen':
-      return <IconCodepen />;
+    case 'Leetcode':
+      return <IconLeetcode />;
     case 'External':
       return <IconExternal />;
     case 'Folder':
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,27 +60,26 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Sanjay Singh Patel.</h2>;
+<<<<<<< HEAD
   const three = <h3 className="big-heading">I build things for the web.</h3>;
+=======
+  const three = <h3>currently exploring things... </h3>;
+>>>>>>> 3e2de76 (.)
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I'm a software developer who specialises in creating (and, on occasion, designing) amazing
+        digital experiences. I'm also a big fan of data analytics.
       </p>
     </>
   );
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="mailto:sanjupatel11112000@gmail.com"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Get in Touch
     </a>
   );
 
